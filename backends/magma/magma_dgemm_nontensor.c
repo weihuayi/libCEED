e1c3b65d06e3a348eb1467b4e76304790bfb953a
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2022-03-08 12:31:34 -0500

    first try at calling gemm selector

--- backends/magma/magma_dgemm_nontensor.c
+++ backends/magma/magma_dgemm_nontensor.c
@@ -16,6 +16,110 @@
 
 #include "ceed-magma.h"
 
+#define MAGMA_USE_GEMM_SELECTOR
+
+#ifdef HAVE_HIP
+#define devblasDgemmStridedBatched     hipblasDgemmStridedBatched
+#define magma_queue_get_devblas_handle magma_queue_get_hipblas_handle
+#define devblas_trans_const            hipblas_trans_const
+#else
+#define devblasDgemmStridedBatched     cublasDgemmStridedBatched
+#define magma_queue_get_devblas_handle magma_queue_get_cublas_handle
+#define devblas_trans_const            cublas_trans_const
+#endif
+
+#ifdef MAGMA_USE_GEMM_SELECTOR
+int
+magma_dgemm_nontensor(
+  magma_trans_t transA, magma_trans_t transB,
+  magma_int_t m, magma_int_t n, magma_int_t k,
+  double alpha, const double *dA, magma_int_t ldda,
+  const double *dB, magma_int_t lddb,
+  double beta,  double *dC, magma_int_t lddc,
+  magma_queue_t queue )
+{
+  magma_int_t nbatch, use_magmablas;
+  magma_int_t arch = magma_getdevice_arch();
+
+  // check for specific transpositions (NN and TN only)
+  bool NN = transA == MagmaNoTrans && transB == MagmaNoTrans;
+  bool TN = transA == MagmaTrans   && transB == MagmaNoTrans;
+  if ( !(NN || TN) ) {
+    // default case -- no specific tuning
+    magma_dgemm(
+      transA, transB, m, n, k,
+      alpha, dA, ldda,
+             dB, lddb,
+      beta,  dC, lddc, queue);
+    return 0;
+  }
+
+  // get tuning decision
+  char       trans = (transA == MagmaNoTrans) ? 'n' : 't';
+  char   precision = 'd';
+  gemm_selector( arch, precision, trans, m, n, k, nbatch, use_magmablas );
+
+  // perform the dgemm operation
+  if ( nbatch == n) {
+    // no batching, do not use magmablas
+    magma_dgemm(
+      transA, transB, m, n, k,
+      alpha, dA, ldda,
+             dB, lddb,
+      beta,  dC, lddc, queue);
+  } else {
+    // use batch kernels
+    magma_int_t batchCount = n/nbatch;
+    magma_int_t n2 = n - (batchCount * nbatch);
+    magma_int_t strideA = 0;
+    magma_int_t strideB = lddb*nbatch;
+    magma_int_t strideC = lddc*nbatch;
+
+    if ( use_magmablas ) {
+      magmablas_dgemm_batched_strided(
+        transA, transB, m, nbatch, k,
+        alpha, dA, ldda, strideA,
+        dB, lddb, strideB,
+        beta,  dC, lddc, strideC,
+        batchCount, queue);
+
+      // cleanup
+      if (n2 > 0) {
+        magma_dgemm(
+          transA, transB, m, n2, k,
+          alpha, dA, ldda,
+          dB + batchCount * strideB, lddb,
+          beta,  dC + batchCount * strideC, lddc, queue);
+      }
+    } else {
+      devblasDgemmStridedBatched(
+        magma_queue_get_devblas_handle( queue ),
+        devblas_trans_const(transA), devblas_trans_const(transB),
+        (int)m, (int)nbatch, (int)k,
+        &alpha, (const double *) dA, (int)ldda, strideA,
+        (const double *) dB, (int)lddb, strideB,
+        &beta,                  dC, (int)lddc, strideC, (int)batchCount );
+
+      // cleanup
+      if (n2 > 0) {
+        devblasDgemmStridedBatched(
+          magma_queue_get_devblas_handle( queue ),
+          devblas_trans_const(transA), devblas_trans_const(transB),
+          (int)m, (int)n2, (int)k,
+          &alpha, (const double *) dA, (int)ldda, strideA,
+          (const double *) dB + batchCount * strideB, (int)lddb, strideB,
+          &beta,           dC + batchCount * strideC, (int)lddc, strideC, 1 );
+      }
+    }
+  }
+
+  // wait for the operation to complete
+  ceed_magma_queue_sync( queue );
+
+  return 0;
+}
+
+#else
 #ifdef HAVE_HIP
 // TODO: Tune for HIP
 int
@@ -25,7 +129,8 @@ magma_dgemm_nontensor(
   double alpha, const double *dA, magma_int_t ldda,
   const double *dB, magma_int_t lddb,
   double beta,  double *dC, magma_int_t lddc,
-  magma_queue_t queue ) {
+  magma_queue_t queue )
+{
 
   // check for specific transpositions (NN and TN only)
   bool NN = transA == MagmaNoTrans && transB == MagmaNoTrans;
@@ -160,7 +265,8 @@ magma_dgemm_nontensor(
   double alpha, const double *dA, magma_int_t ldda,
   const double *dB, magma_int_t lddb,
   double beta,  double *dC, magma_int_t lddc,
-  magma_queue_t queue ) {
+  magma_queue_t queue )
+{
 
   // check for specific transpositions (NN and TN only)
   bool NN = transA == MagmaNoTrans && transB == MagmaNoTrans;
@@ -286,3 +392,4 @@ magma_dgemm_nontensor(
   return 0;
 }
 #endif
+#endif    // MAGMA_USE_GEMM_SELECTOR
